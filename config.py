900f01fb23dbc6b83a79ce7d654a2c70b3aa369a
Trevlix <83665403+trevlix-booking@users.noreply.github.com>
2022-08-04 14:01:44 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# IMPORTANT
+# change the following variables to your actual API credentials
+
+api_json_url = "https://book.trevlix.com/api/v1/"
+api_key_username = "YOUR-API-KEY-USERNAME"
+api_key_password = "YOUR-API-KEY-PASSWORD"
